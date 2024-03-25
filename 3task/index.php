8f122637ae06a6dcef6cd3af6048a13a27d91b27
IraTkhagapso14 <144713252+IraTkhagapso14@users.noreply.github.com>
2024-03-25 14:20:12 +0300

    Update index.php

--- 3task/index.php
+++ 3task/index.php
@@ -102,9 +102,9 @@ if ($errors) {
     exit();
 }
 
-$user = 'u67313';
-$pass = '4344635';
-$db = new PDO('mysql:host=localhost;dbname=u67313', $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+$user = 'u67438';
+$pass = '9231297';
+$db = new PDO('mysql:host=localhost;dbname=u67438', $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 foreach ($_POST['languages'] as $language) {
